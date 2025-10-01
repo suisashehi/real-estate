a34505b7b883f3d43e4ad50b039f2a607802fb09
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-02 00:38:56 +0200

    Setup base styles and import header.css with utility classes

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,7 @@ import Header from './components/Header/Header'
 function App() {
   return (
     <div className="App">
-      <section className="h-wrapper">
-        <div className="h-container">
-          <img src="./logo.png" alt="logo" width={100} />
-
-          <div className="h-menu">
-            <a 
-            href=""
-            >
-              Residencies
-
-            </a>
-
-            <a href="">Our Value</a>
-            <a href="">Contact Us</a>
-            <a href="">Get Started</a>
-            <button className='button'>
-            <a href="">Contact</a>
-            </button>
-          </div>
-        </div>
-      </section>
+      <Header />
       
     </div>
   );
