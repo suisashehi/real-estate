fa309a6e8f1b58874d3e1a36c5ef16ebdac0e880
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-02 23:26:54 +0200

    Added title, description, and placeholder search bar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,23 @@ const Hero = () => {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter hero-container">
-        <div className="hero-left">Left section</div>
+        {/* left side */}
+        <div className="hero-left">
+          <div className="hero-title">
+            <h1>
+              Discover <br /> Most Suitable <br /> Property{" "}
+            </h1>
+          </div>
+
+          <div className="flexColStart hero-des">
+            <span>Find a variety of properties that suit  you very easilty  </span>
+              <span>Forget all difficulties in finding  a residence for you</span>
+          </div>
+          <div className="search-bar">
+            search bar 
+          </div>
+        </div>
+        {/* right side */}
 
         <div className="flexCenter hero-right">
           <div className="image-container">
