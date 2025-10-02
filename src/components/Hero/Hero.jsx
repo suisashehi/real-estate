ec0590e3b0fe9863961c96d03227770e58a70346
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-02 23:52:29 +0200

    Added location icon, input, and button inside search bar and background, border, padding, and removed input border/outline

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
 
 const Hero = () => {
   return (
@@ -8,22 +9,25 @@ const Hero = () => {
         {/* left side */}
         <div className="flexColStart hero-left">
           <div className="hero-title">
-            <div className="orange-circle"/>
+            <div className="orange-circle" />
             <h1>
-              Discover <br /> Most Suitable <br /> Property{" "}
+              Discover <br /> Most Suitable <br /> Property
             </h1>
           </div>
 
           <div className="flexColStart hero-des">
-            <span>Find a variety of properties that suit  you very easilty  </span>
-              <span>Forget all difficulties in finding  a residence for you</span>
+            <span>Find a variety of properties that suit you very easily</span>
+            <span>Forget all difficulties in finding a residence for you</span>
           </div>
+
           <div className="search-bar">
-            search bar 
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
           </div>
         </div>
-        {/* right side */}
 
+        {/* right side */}
         <div className="flexCenter hero-right">
           <div className="image-container">
             <img src="/hero-image.png" alt="hero" />
