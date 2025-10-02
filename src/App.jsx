79e8385e4b743b7235aed588677de64e6ac62178
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-02 23:09:48 +0200

    Applied flexbox, spacing, and background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import Header from './components/Header/Header'
+import Hero from "./components/Hero/Hero.jsx";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />      
       
     </div>
   );
