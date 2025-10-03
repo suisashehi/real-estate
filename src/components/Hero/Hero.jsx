a20f924f5b066f572de4e1fa8c1d7f37a00af0d3
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-03 21:56:42 +0200

    Fixed Hero search input bar (removed border/outline, aligned with flexCenter) and adjusted search bar width to 100% for better fit.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
             <span>Forget all difficulties in finding a residence for you</span>
           </div>
 
-          <div className="search-bar">
+          <div className="flexCenter search-bar">
             <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
