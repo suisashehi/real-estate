346d6d315a2e1bda7a2ba3b9d218ad9fea809e36
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-03 22:25:24 +0200

    Added stats section below search bar with react-countup for animated numbers (Premium Products, Happy Customers, Award Winning).

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -28,34 +28,34 @@ const Hero = () => {
           </div>
 
           <div className="flexCenter stats">
-            <div className="flexColStart stat">
+            <div className="flexColCenter stat">
               <span>
                 <CountUp start={8800} end={9000} duration={4}/>
                 <span>+</span>
               </span>
               
-              <span>
+              <span className="secondaryText">
                 Premiun Products
               </span>
             </div>
-            <div className="flexColStart stat">
+            <div className="flexColCenter stat">
               <span>
                 <CountUp start={1950} end={2000} duration={4}/>
                 <span>+</span>
               </span>
               
-              <span>
+              <span className="secondaryText">
                 Happy Customers
               </span>
             </div>
 
-             <div className="flexColStart stat">
+             <div className="flexColCenter stat">
               <span>
                 <CountUp end={28}/>
                 <span>+</span>
               </span>
               
-              <span>
+              <span className="secondaryText">
                 Award Winning 
               </span>
             </div>
