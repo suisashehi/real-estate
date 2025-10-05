14d64c82f1e094f461e757450997fce33dfd15c3
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-06 00:32:06 +0200

    refined navigation button design with complete styling, colors, shadows, and finalized slider functionality.

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -42,7 +42,7 @@ const SliderButtons = () => {
   const swiper = useSwiper();
 
   return (
-    <div className="r-buttons">
+    <div className= "flexCenter r-buttons">
       <button onClick={()=>swiper.slidePrev()}>&lt;</button>
       <button onClick={()=>swiper.slideNext()}>&gt;</button>
     </div>
