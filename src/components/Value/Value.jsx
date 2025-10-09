1fe59aba710a2f359dc0aa53f7bc8eb3eb1ffef4
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-10 00:25:06 +0200

    integrated AccordionItemState for dynamic expanded/collapsed classes and styled expanded items with shadows and rounded borders for a polished look.

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,76 +1,77 @@
-import React from 'react'
-import{
-    Accordion,
-    AccordionItem,
-    AccordionItemHeading,
-    AccordionItemButton,
-    AccordionItemPanel,
-    AccordionItemState
+import React, { useState } from 'react'
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+  AccordionItemState,
 } from 'react-accessible-accordion'
-import 'react-accessible-accordion/dist/fancy-example.css';
-import {MdOutlineArrowDropDown} from 'react-icons/md'
+import 'react-accessible-accordion/dist/fancy-example.css'
+import { MdOutlineArrowDropDown } from 'react-icons/md'
 import './Value.css'
 import data from '../../utils/accordion'
+
 const Value = () => {
   return (
     <section className="v-wrapper">
-        <div className="paddings innerWidth flexCenter v-container">
-            {/* left side */}
-            <div className="v-left">
-                <div className="image-container">
-                    <img src="./value.png" alt=""/>
-                </div> 
-                </div> 
-            {/* right side */}
-            <div className="flexColStart v-right">
-                <span className="orangeText">Our Value</span>
-                <span className="primaryText">Value We Give to You</span>
+      <div className="paddings innerWidth flexCenter v-container">
+        {/* left side */}
+        <div className="v-left">
+          <div className="image-container">
+            <img src="./value.png" alt="value" />
+          </div>
+        </div>
 
-                <span className="secondaryText">
-                    We always ready to help by providing the best services for you. 
-                    <br/>
-                    We believe a good place to live can make your life better</span>
+        {/* right side */}
+        <div className="flexColStart v-right">
+          <span className="orangeText">Our Value</span>
+          <span className="primaryText">Value We Give to You</span>
+          <span className="secondaryText">
+            We always ready to help by providing the best services for you.
+            <br />
+            We believe a good place to live can make your life better.
+          </span>
 
-                    <Accordion
-                    allowMultipleExpanded={false}
-                    preExpanded={[0]}
-                    className="accordion"
-                    >
-                       {
-                          data.map((item, i)=>{
-                            return(
-                                <AccordionItem className="accordionItem" key={i} uuid={i}>
-                                <AccordionItemHeading>
-                                    <AccordionItemButton className="flexCenter accordionButton">
+          <Accordion allowMultipleExpanded={false} preExpanded={[0]}>
+            {data.map((item, i) => {
+              const [className, setClassName] = useState('collapsed')
+              return (
+                <AccordionItem
+                  key={i}
+                  uuid={i}
+                  className={`accordionItem ${className}`}
+                >
+                  <AccordionItemHeading>
+                    <AccordionItemButton className="flexCenter accordionButton">
+                      <AccordionItemState>
+                        {({ expanded }) => {
+                          setTimeout(() => {
+                            setClassName(expanded ? 'expanded' : 'collapsed')
+                          }, 0)
+                          return null
+                        }}
+                      </AccordionItemState>
 
-                                    <AccordionsItemState>
-                                        {({expanded})=> expanded ? setClassName("expanded") : setClassName("collapsed")}
-                                    </AccordionsItemState>
+                      <div className="flexCenter icon">{item.icon}</div>
+                      <span className="primaryText">{item.heading}</span>
+                      <div className="flexCenter icon arrowIcon">
+                        <MdOutlineArrowDropDown size={20} />
+                      </div>
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
 
-                                        <div className="flexCenter icon">
-                                            {item.icon}
-                                        </div>
-                                        <span className="primaryText">{item.heading}</span>
-                                        <div className="flexCenter icon">
-                                            <MdOutlineArrowDropDown size={20}/>
-                                        </div>
-                                    </AccordionItemButton>
-                                </AccordionItemHeading>
-                                <AccordionItemPanel>
-                                    <p className="secondaryText">{item.detail}</p>
-                                </AccordionItemPanel>
-                                       
-
-                                </AccordionItem>
-                            );
-                          })
-                       } 
-                        
-                    </Accordion>
-            </div>
-        </div>    
+                  <AccordionItemPanel>
+                    <p className="secondaryText">{item.detail}</p>
+                  </AccordionItemPanel>
+                </AccordionItem>
+              )
+            })}
+          </Accordion>
+        </div>
+      </div>
     </section>
   )
 }
 
-export default Value 
+export default Value
