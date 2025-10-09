cb00fdc18027636b60202302a26894450d8786fd
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-09 23:12:14 +0200

    added section headings, descriptive spans, and applied base CSS for container, flex layout, and balanced left-right distribution.

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -18,8 +18,18 @@ const Value = () => {
             <div className="v-left">
                 <div className="image-container">
                     <img src="./value.png" alt=""/>
-                </div>
                 </div> 
+                </div> 
+            {/* right side */}
+            <div className="flexColStart v-right">
+                <span className="orangeText">Our Value</span>
+                <span className="primaryText">Value We Give to You</span>
+
+                <span className="secondaryText">
+                    We always ready to help by providing the best services for you. 
+                    <br/>
+                    We believe a good place to live can make your life better</span>
+            </div>
         </div>    
     </section>
   )
