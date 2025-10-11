9d8aa51505e6f208e67f5e5bc7e20604ec44a96f
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-12 00:44:50 +0200

    implemented left and right sections with image container and headings for Contact component.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import './Contact.scss'
+import './Contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="c-wrapper">
+        <div className="paddings innerWidth flexCenter c-container">
+            {/* left side */}
+            <div className="c-left">
+                left
+            </div>
+            {/* right side */}
+            <div className="c-right">
+                <div className="image-container">
+                    <img src="./contact.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
