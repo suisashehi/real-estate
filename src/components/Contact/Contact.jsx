f5499a5bc10d7af8a73f319036bee20df4686f76
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-12 01:11:00 +0200

    structured Contact left section with spans, flex layout, and initial gap styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,8 +6,10 @@ const Contact = () => {
     <section className="c-wrapper">
         <div className="paddings innerWidth flexCenter c-container">
             {/* left side */}
-            <div className="c-left">
-                left
+            <div className="flexColStart c-left">
+                <span className='orangeText'>Our Contacts</span>
+                <span className='primaryText'>Easy to contact us</span>
+                <span className='secondaryText'>We always ready to help by providing the best service for you. Wr believe a good place to live can make your life better</span>
             </div>
             {/* right side */}
             <div className="c-right">
