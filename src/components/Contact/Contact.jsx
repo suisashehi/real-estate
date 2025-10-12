22fd894f3ef5900aca88dcd895b73b66d0673679
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-12 12:29:18 +0200

    added first contact mode with icon, details, and button including necessary icon imports.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
+import { MdCall } from "react-icons/md";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi2";
 
 const Contact = () => {
   return (
     <section className="c-wrapper">
-        <div className="paddings innerWidth flexCenter c-container">
-            {/* left side */}
-            <div className="flexColStart c-left">
-                <span className='orangeText'>Our Contacts</span>
-                <span className='primaryText'>Easy to contact us</span>
-                <span className='secondaryText'>We always ready to help by providing the best service for you. Wr believe a good place to live can make your life better</span>
-            </div>
-            {/* right side */}
-            <div className="c-right">
-                <div className="image-container">
-                    <img src="./contact.jpg" alt="" />
-                </div>
-            </div>
-        </div>
-    </section>
-  )
-}
+      <div className="paddings innerWidth flexCenter c-container">
+        {/* left side */}
+        <div className="flexColStart c-left">
+          <span className="orangeText">Our Contacts</span>
+          <span className="primaryText">Easy to contact us</span>
+          <span className="secondaryText">
+            We always ready to help by providing the best service for you. Wr
+            believe a good place to live can make your life better
+          </span>
 
-export default Contact
+          <div className="flexColStart contactModes">
+            {/* first row */}
+            <div className="flexColStart row">
+              <div className="flexcColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <MdCall size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span>Call</span>
+                    <span>021 123 154 17</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">Call Now</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* right side */}
+        <div className="c-right">
+          <div className="image-container">
+            <img src="./contact.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
