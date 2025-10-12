a1ffef7348fe4c483310d13fe2e711c427bb0f88
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-12 13:43:29 +0200

    implemented Footer right section with information spans and navigation menu items.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <section className="f-wrapper">
-      <div className="paddings innerWidth flexCenter f-Container">
+      <div className="paddings innerWidth flexCenter f-container">
         {/* left side */}
         <div className="flexColStart f-left">
           <img src="./logo2.png" alt="" width={120} />
@@ -13,6 +13,18 @@ const Footer = () => {
             Our vision is to make all people <br />
             the best place to live for them.
           </span>
+        </div>
+
+        <div className="flexColStart f-right">
+          <span className="primaryText">Information</span>
+          <span className="secondaryText">145 New York, FL 4571, USA</span>
+
+          <div className="flexCenter f-menu">
+            <span>Property</span>
+            <span>Services</span>
+            <span>Product</span>
+            <span>About us</span>
+          </div>
         </div>
       </div>
     </section>
