19c5e36896a60eeb6eb6064071fa1b8cd8acd617
Suisa Shehi <suisa_sh20@icloud.com>
2025-10-15 23:30:34 +0200

    ntegrated Framer Motion library and added entrance animation for Hero heading with smooth y-axis and opacity transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import Header from './components/Header/Header'
+import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero.jsx";
-import './App.css'
-import Companies from './components/Companies/Companies.jsx';
-import Residencies from './components/Residencies/Residencies.jsx';
-import Value from './components/Value/Value.jsx';
-import Contact from './components/Contact/Contact.jsx';
-import GetStarted from './components/GetStarted/GetStarted.jsx';
-import Footer from './components/Footer/Footer.jsx';
+import "./App.css";
+import Companies from "./components/Companies/Companies.jsx";
+import Residencies from "./components/Residencies/Residencies.jsx";
+import Value from "./components/Value/Value.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import GetStarted from "./components/GetStarted/GetStarted.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <div className="white-gradient"/>
+        <div className="white-gradient" />
 
-      <Header />
-     <Hero />   
+        <Header />
+        <Hero />
       </div>
-    
+
       <Companies />
       <Residencies />
       <Value />
       <Contact />
       <GetStarted />
       <Footer />
-      
     </div>
   );
 }
-
 
 export default App;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
-
+import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <section className="hero-wrapper">
@@ -11,9 +11,16 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            <h1>
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                type: "spring",
+              }}
+            >
               Discover <br /> Most Suitable <br /> Property
-            </h1>
+            </motion.h1>
           </div>
 
           <div className="flexColStart hero-des">
